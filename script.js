88eea25c2288a9e3e3ecfac02255a2873c2b9ab6
Đorđe Ražnatović <djokole2@gmail.com>
2017-12-22 13:35:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
-const hoursHand = document.querySelector('.hour-hand');
+const hourHand = document.querySelector('.hour-hand');
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
@@ -10,14 +10,17 @@ function setDate() {
 	} else { secondHand.style.transition = 'all 0.05s';};
 	const minutes = now.getMinutes();
 	const minutesDegrees = ((minutes / 60) * 360) + 90;
-	if (minutesDegrees >= 444) {
+	if (minutesDegrees == 90) {
 		minHand.style.transition = 'all 0s';
 	} else { minHand.style.transition = 'all 0.05s';};
 	const hours = now.getHours();
 	const hoursDegrees = ((hours / 12) * 360) + 90;
+	if (hoursDegrees == 90) {
+		hourHand.style.transition = 'all 0s';
+	} else { hourHand.style.transition = 'all 0.05s';};
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 	minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 }
 
